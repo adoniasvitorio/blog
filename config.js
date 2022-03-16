a6c528634d34aadf072f74476c47d8d7aeb73024
Adonias Vitorio <adonias.vitorio@gmail.com>
2022-03-15 22:05:50 -0300

    Update Configs

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: 'by Adonias Vitorio',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'G-MHDVWWGM96',
   useKatex: false,
   menu: [
     {
